2a65ee9962b0f73bb71bf37b62ab8882707ace4c
GabrielKrauss2025 <al.gabriel.dias@impatech.edu.br>
2025-10-21 20:53:53 -0300

    Adiciona a classe de interpolação polinomial (Lagrange)

--- CB2325NumericaG5/interpolacao.py
+++ CB2325NumericaG5/interpolacao.py
@@ -28,3 +28,46 @@ class PoliInterp:
             return temp
         else:
             return f'{temp:.4f}'
+
+
+class IntLinear:
+
+    def __init__(self, dominio, imagem):
+        self.x = symbols('x')
+        # Garantimos que o domínio e a imagem possuem a mesma quantidade de pontos
+        self.pares_ord = []
+        for i, e in zip(dominio, imagem):
+            self.pares_ord.append((i, e))
+        self.pares_ord = sorted(self.pares_ord)
+
+        # Criamos um dicionário para dividir as retas que ligam os pontos 2 a 2
+        self.pol = {}
+        for i in range(len(self.pares_ord) - 1):
+            reta = self.pares_ord[i][1] + (self.x - self.pares_ord[i][0]) * (
+                        (self.pares_ord[i + 1][1] - self.pares_ord[i][1]) / (
+                            self.pares_ord[i + 1][0] - self.pares_ord[i][0]))
+            self.pol[(self.pares_ord[i][0], self.pares_ord[i + 1][0])] = simplify(reta)
+
+    def __repr__(self):
+        return f'{self.pol}'
+
+    def eval(self, pos, t):
+        return self.pol[pos].subs(self.x, t)
+
+    def __call__(self, t):
+        # Lista dos x's
+        temp = [i[0] for i in self.pares_ord]
+
+        for i in range(len(temp) - 1):
+            if temp[i] <= t <= temp[i + 1]:
+                return self.eval((temp[i], temp[i + 1]), t)
+
+        if t > temp[-1]:
+            return self.eval((temp[-2], temp[-1]), t)
+
+        else:
+            return self.eval((temp[0], temp[1]), t)
+
+
+
+
