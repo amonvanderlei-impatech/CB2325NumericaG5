0141d34ea9898f7d8fda12054e8fdf4aa42c376e
GabrielKrauss2025 <al.gabriel.dias@impatech.edu.br>
2025-10-22 08:46:39 -0300

    Adiciona defesa nas classes para verificação do domínio

--- CB2325NumericaG5/interpolacao.py
+++ CB2325NumericaG5/interpolacao.py
@@ -3,22 +3,34 @@ from sympy import symbols, simplify
 class PoliInterp:
     '''Interpola os pontos dados utilizando o metodo de Lagrange e armazena o polinômio simplificado'''
 
+    @staticmethod
+    def dom_valido(dominio):
+        temp = set(dominio)
+        if len(temp) != len(dominio):
+            return False
+        else:
+            return True
+
     def __init__(self, dominio, imagem):
-        self.x = symbols('x')
-        # Garantimos que o domínio e a imagem possuem a mesma quantidade de pontos
-        self.dominio = []
-        self.imagem = []
-        for i, e in zip(dominio, imagem):
-            self.dominio.append(i)
-            self.imagem.append(e)
-        soma = 0
-        for i in range(len(self.dominio)):
-            prod = 1
-            for e in range(len(self.imagem)):
-                if e != i:
-                    prod *= (self.x - self.dominio[e]) / (self.dominio[i] - self.dominio[e])
-            soma += self.imagem[i] * prod
-        self.pol = simplify(soma)
+        if not PoliInterp.dom_valido(dominio):
+            raise ValueError('Domínio inválido')
+
+        else:
+            self.x = symbols('x')
+            # Garantimos que o domínio e a imagem possuem a mesma quantidade de pontos
+            self.dominio = []
+            self.imagem = []
+            for i, e in zip(dominio, imagem):
+                self.dominio.append(i)
+                self.imagem.append(e)
+            soma = 0
+            for i in range(len(self.dominio)):
+                prod = 1
+                for e in range(len(self.imagem)):
+                    if e != i:
+                        prod *= (self.x - self.dominio[e]) / (self.dominio[i] - self.dominio[e])
+                soma += self.imagem[i] * prod
+            self.pol = simplify(soma)
 
     def __repr__(self):
         return f'{self.pol}'
@@ -36,22 +48,34 @@ class IntLinear:
         e armazena elas em um dicionário interno dividindo
          cada reta por intervalo'''
 
+    @staticmethod
+    def dom_valido(dominio):
+        temp = set(dominio)
+        if len(temp) != len(dominio):
+            return False
+        else:
+            return True
 
     def __init__(self, dominio, imagem):
-        self.x = symbols('x')
-        # Garantimos que o domínio e a imagem possuem a mesma quantidade de pontos
-        self.pares_ord = []
-        for i, e in zip(dominio, imagem):
-            self.pares_ord.append((i, e))
-        self.pares_ord = sorted(self.pares_ord)
+        if not IntLinear.dom_valido(dominio):
+            raise ValueError('Domínio inválido')
+        else:
+            from sympy import symbols, simplify
+            self.x = symbols('x')
 
-        # Criamos um dicionário para dividir as retas que ligam os pontos 2 a 2
-        self.pol = {}
-        for i in range(len(self.pares_ord) - 1):
-            reta = self.pares_ord[i][1] + (self.x - self.pares_ord[i][0]) * (
+            # Garantimos que o domínio e a imagem possuem a mesma quantidade de pontos
+            self.pares_ord = []
+            for i, e in zip(dominio, imagem):
+                self.pares_ord.append((i, e))
+            self.pares_ord = sorted(self.pares_ord)
+
+            # Criamos um dicionário para dividir as retas que ligam os pontos 2 a 2
+            self.pol = {}
+            for i in range(len(self.pares_ord) - 1):
+                reta = self.pares_ord[i][1] + (self.x - self.pares_ord[i][0]) * (
                         (self.pares_ord[i + 1][1] - self.pares_ord[i][1]) / (
-                            self.pares_ord[i + 1][0] - self.pares_ord[i][0]))
-            self.pol[(self.pares_ord[i][0], self.pares_ord[i + 1][0])] = simplify(reta)
+                        self.pares_ord[i + 1][0] - self.pares_ord[i][0]))
+                self.pol[(self.pares_ord[i][0], self.pares_ord[i + 1][0])] = simplify(reta)
 
     def __repr__(self):
         return f'{self.pol}'
