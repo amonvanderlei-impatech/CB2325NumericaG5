6372f9fdd9726dd44696e99ad9d43782785db072
GabrielKrauss2025 <al.gabriel.dias@impatech.edu.br>
2025-10-29 17:50:42 -0300

    Melhora a verificação de caso

--- CB2325NumericaG5/interpolacao.py
+++ CB2325NumericaG5/interpolacao.py
@@ -1,40 +1,74 @@
-from sympy import symbols, simplify, Number
+from sympy import symbols, simplify, Number, latex, Symbol
+from typing import Union
+import numbers
+
 
 class Interpolacao:
-    def __init__(self, dominio, imagem, imagem_derivada = None):
-        if imagem_derivada is None:
-            # Garantimos que o domínio e a imagem são listas de pontos
-            if not isinstance(dominio, list) or not isinstance(imagem, list):
-                raise ValueError('Argumentos inválidos')
+    """
+        Classe abstrata que verifica a entrada do domínio, imagem e imagem_derivada.
 
-            # Garantimos que o domínio e a imagem possuem a mesma quantidade de pontos
-            if len(dominio) != len(imagem):
-                raise ValueError('Dados inválidos')
+        Args:
+            dominio (list): Lista de pontos do domínio
+            imagem (list): Lista de pontos da imagem
+            imagem_derivada (list): Lista de pontos da imagem da derivada. Padroniza em None
 
-            # Garantimos que o domínio não possui pontos repetidos
-            if len(set(dominio)) != len(dominio) or len(dominio) < 2:
-                raise ValueError('Domínio inválido')
+        Raises:
+            TypeError: se o 'dominio' e a 'imagem' não forem listas
+            ValueError: se o tamanho do 'dominio' for diferente do tamanho da 'imagem'
+            TypeError: se o 'dominio', a 'imagem' e a 'imagem_derivada' não forem listas
+            ValueError: se o tamanho do 'dominio', da 'imagem' ou da 'imagem_derivada' forem diferentes
+            ValueError: se o 'dominio' possuir valores repetidos ou quantidade insuficiente de pontos
+            NotImplementedError: se a __repr__ ou o __call__ não forem implementados
+        """
 
-        else:
-            if not isinstance(dominio, list) or not isinstance(imagem, list) or not isinstance(imagem_derivada, list):
-                raise ValueError('Argumentos inválidos')
+    def __init__(self, dominio:list, imagem:list, imagem_derivada:list = None):
+        # Garantimos que o domínio e a imagem são listas de pontos
+        if not isinstance(dominio, list) or not isinstance(imagem, list):
+            raise TypeError('`dominio` e `imagem` devem ser do tipo list')
 
-            if len(dominio) != len(imagem) or len(dominio) != len(imagem_derivada) or len(imagem) != len(
-                    imagem_derivada):
-                raise ValueError('Dados inválidos')
+        # Garantimos que o domínio e a imagem possuem a mesma quantidade de pontos
+        if len(dominio) != len(imagem):
+            raise ValueError('`dominio` e `imagem` devem ter a mesma quantidade de pontos')
 
-            if len(set(dominio)) != len(dominio) or len(dominio) < 2:
-                raise ValueError('Domínio inválido')
+        # Garantimos que o domínio não possui pontos repetidos
+        if len(set(dominio)) != len(dominio):
+            raise ValueError('`dominio` não pode ter valores repetidos')
 
-            self.imagem_derivada = imagem_derivada
+        # Garantimos que o domínio possui mais de 2 pontos
+        if len(dominio) < 2:
+            raise ValueError('`dominio` deve possuir mais de 2 pontos')
 
-            if len(set(dominio)) != len(dominio):
-                raise ValueError('Dados inválidos')
+        # Garantimos que o domínio é uma lista de números
+        for i in dominio:
+            if not isinstance(i, numbers.Real):
+                raise TypeError('`dominio` deve ser uma lista de números')
 
+        # Garantimos que a imagem é uma lista de números
+        for i in imagem:
+            if not isinstance(i, numbers.Real):
+                raise TypeError('`imagem` deve ser uma lista de números')
+
+        # Cria as variáveis internas
         self.x = symbols('x')
         self.dominio = dominio
         self.imagem = imagem
 
+        if imagem_derivada is not None:
+            # Garantimos que a imagem_derivada é uma lista de pontos
+            if not isinstance(imagem_derivada, list):
+                raise TypeError('`imagem_derivada` deve ser do tipo list')
+
+            # Garantimos que o domínio e a imagem_derivada possuem a mesma quantidade de pontos
+            if len(dominio) != len(imagem_derivada):
+                raise ValueError('`dominio` e `imagem` devem ter a mesma quantidade de pontos')
+
+            # Garantimos que a imagem_derivada é uma lista de números
+            for i in imagem_derivada:
+                if not isinstance(i, numbers.Real):
+                    raise TypeError('`imagem_derivada` deve ser uma lista de números')
+
+            # Cria uma variável interna
+            self.imagem_derivada = imagem_derivada
 
     def __repr__(self):
         raise NotImplementedError
@@ -44,17 +78,22 @@ class Interpolacao:
 
 
 class PoliInterp(Interpolacao):
-    """Interpola os pontos dados utilizando o metodo de Lagrange e armazena o polinômio simplificado
+    """
+        Interpola os pontos dados utilizando o metodo de Lagrange e armazena o polinômio simplificado
     Args:
-        dominio (list): Lista de pontos do domínio.
-        imagem (list): Lista de pontos da imagem.
+        dominio (list): Lista de pontos do domínio
+        imagem (list): Lista de pontos da imagem
 
     Raises:
-        ValueError: Se o domínio possuir pontos iguais.
+        ValueError: se o ponto não for do tipo int, float ou Symbol
+        ValueError: se o ponto estiver fora do intervalo de interpolação, evitando extrapolação
 
     Returns:
-        representação do polinômio interpolador,
-        float: valor do polinômio num ponto específico."""
+        str: representação do polinômio interpolador
+        str: representação do polinômio interpolador em LaTeX
+        int: valor do polinômio num ponto específico, se o valor for inteiro
+        float: valor do polinômio num ponto específico, se o valor for um número de ponto flutuante
+    """
 
     def __init__(self, dominio, imagem):
         super().__init__(dominio, imagem)
@@ -71,30 +110,46 @@ class PoliInterp(Interpolacao):
         self.pol = simplify(soma)  # Polinômio interpolador simplificado
 
     def __repr__(self):
-        # A representação é uma str do polinômio
         return f'{self.pol}'
 
-    def __call__(self, t):
-        # Previne extrapolação (valores fora do intervalo do domínio não são bem aproximados)
-        if t < min(self.dominio) or t > max(self.dominio):
-            raise ValueError('Extrapolação')
+    def __call__(self, p:Union[int, float, Symbol]):
+        if not isinstance(p, numbers.Real) and not isinstance(p, Symbol):
+            raise ValueError('O ponto deve ser um número real ou uma variável')
 
-        temp = self.pol.subs(self.x, t)
+        # Retorna a representação do polinômio no ponto p (variável) em LaTeX
+        if isinstance(p, Symbol):
+            return latex(self.pol.subs(self.x, p))
+
+        # Previne extrapolação
+        if p < min(self.dominio) or p > max(self.dominio):
+            raise ValueError('Valores fora do intervalo do domínio não são bem aproximados')
+
+        temp = self.pol.subs(self.x, p)
         if isinstance(temp, Number):
             if temp.is_integer:
                 return int(temp)
             return float(temp)
-
         return None
 
 
-
-
-
 class InterpLinear(Interpolacao):
-    """Cria retas que interpolam pontos dois a dois
-        e armazena elas em um dicionário interno dividindo
-         cada reta por intervalo"""
+    """
+        Cria retas que interpolam pontos dois a dois e armazena elas num dicionário interno dividindo cada reta por intervalo.
+
+        Args:
+            dominio (list): Lista de pontos do domínio
+            imagem (list): Lista de pontos da imagem
+
+        Raises:
+            ValueError: se o ponto não for do tipo int ou float
+            ValueError: se o ponto estiver fora do intervalo de interpolação, evitando extrapolação
+
+        Returns:
+            dict: representação das retas interpoladoras por partes
+            int: valor da reta num ponto específico, se o valor for inteiro
+            float: valor da reta num ponto específico, se o valor for um número de ponto flutuante
+
+    """
 
     def __init__(self, dominio, imagem):
         super().__init__(dominio, imagem)
@@ -106,17 +161,20 @@ class InterpLinear(Interpolacao):
 
         # Criamos um dicionário para dividir as retas que ligam os pontos 2 a 2
         self.pol = {}
+
+        # Calcula cada reta
         for i in range(len(self.pares_ord) - 1):
             reta = self.pares_ord[i][1] + (self.x - self.pares_ord[i][0]) * (
                     (self.pares_ord[i + 1][1] - self.pares_ord[i][1]) / (
                     self.pares_ord[i + 1][0] - self.pares_ord[i][0]))
+
+            # Adiciona a reta simplificada no dicionário: (xi, xi+1): reta
             self.pol[(self.pares_ord[i][0], self.pares_ord[i + 1][0])] = simplify(reta)
 
     def __repr__(self):
-        # Retorna um dicionário
         return f'{self.pol}'
 
-    def eval(self, pos, t):
+    def _eval(self, pos:tuple, t:Union[int, float]):
         temp = self.pol[pos].subs(self.x, t)
         if isinstance(temp, Number):
             if temp.is_integer:
@@ -124,24 +182,44 @@ class InterpLinear(Interpolacao):
             return float(temp)
         return None
 
-    def __call__(self, t):
-        # Lista dos x's
+    def __call__(self, p:Union[int, float]):
+        if not isinstance(p, numbers.Real):
+            raise ValueError('O ponto deve ser um número real')
+
         temp = [i[0] for i in self.pares_ord]
 
-        if t>temp[-1] or t<temp[0]:
-            # Extrapolação
-            raise ValueError('Extrapolação')
+        # Extrapolação
+        if p>temp[-1] or p<temp[0]:
+            raise ValueError('Valores fora do intervalo do domínio não são bem aproximados')
 
         for i in range(len(temp) - 1):
-            if temp[i] <= t <= temp[i + 1]:
-                return self.eval((temp[i], temp[i + 1]), t)
+            if temp[i] <= p <= temp[i + 1]:
+                return self._eval((temp[i], temp[i + 1]), p)
         return None
 
 
-
-
 class PoliHermite(Interpolacao):
-    """Interpola utilizando o metodo de Hermite (interpola nos pontos dados e na primeira derivada)"""
+    """
+        Interpola utilizando o metodo de Hermite e armazena o polinomio simplificado
+
+        Args:
+            dominio (list): Lista de pontos do domínio
+            imagem (list): Lista de pontos da imagem
+            imagem_derivada (list): Lista de pontos da imagem da derivada
+
+        Raises:
+            ValueError: se o ponto não for do tipo int, float ou Symbol
+            ValueError: se o ponto estiver fora do intervalo de interpolação, evitando extrapolação
+
+        Returns:
+            str: representação do polinômio interpolador
+            str: primeiro coeficiente de Hermite
+            str: segundo coeficiente de Hermite
+            str: polinômio de Hermite
+            str: representação do polinômio interpolador em LaTeX
+            int: valor do polinômio num ponto específico, se o valor for inteiro
+            float: valor do polinômio num ponto específico, se o valor for um número de ponto flutuante
+        """
 
     def __init__(self, dominio, imagem, imagem_derivada):
         super().__init__(dominio, imagem, imagem_derivada)
@@ -160,7 +238,6 @@ class PoliHermite(Interpolacao):
 
 
     def __repr__(self):
-        # Retorna a representação do polinômio simplificado
         return f'{self.pol}'
 
     def _hx_j(self, j):
@@ -177,13 +254,22 @@ class PoliHermite(Interpolacao):
             pol += self.imagem[j]*self._hx_j(j) + self.imagem_derivada[j]*self._hy_j(j)
         return simplify(pol)
 
-    def __call__(self, t):
-        if min(self.dominio) <= t <= max(self.dominio):
-            temp = self.pol.subs(self.x, t)
+    def __call__(self, p:Union[int, float, Symbol]):
+        if not isinstance(p, numbers.Real) and not isinstance(p, Symbol):
+            raise ValueError('O ponto deve ser um número ou uma variável')
+
+        # Retorna a representação do polinômio no ponto p (variável) em LaTeX
+        if isinstance(p, Symbol):
+            return latex(self.pol.subs(self.x, p))
+
+        # Evita extrapolação
+        if min(self.dominio) <= p <= max(self.dominio):
+            temp = self.pol.subs(self.x, p)
             if isinstance(temp, Number):
                 if temp.is_integer:
                     return int(temp)
                 return float(temp)
             return None
+
         else:
-            raise ValueError('Extrapolação')
+            raise ValueError('Valores fora do intervalo do domínio não são bem aproximados')
