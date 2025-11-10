00f32c4ed34d986a8b2469743e0a4d0bbec1652b
Davi Pestana <al.davi.campos@impatech.edu.br>
2025-11-10 12:29:05 -0300

    Atualização andamento
    Comitts regulares de andamento devido trabalho simultâneo e síncrono com Andre.

--- tests/test_integracao.py
+++ tests/test_integracao.py
@@ -3,9 +3,44 @@ import pytest
 import math
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from CB2325NumericaG5 import integracao
+from CB2325NumericaG5.integracao import integral 
+#Talvez precise importar função gráfico tbm...
 
-#Testes Básicos Primeiro de Funcionamento
+
+
+
+#Testes Básicos de Funcionamento Simples (polinomios)
+@pytest.mark.parametrize(
+    "funcao, a, b, esperado, metodo",
+    [
+        # (função, limite inferior, limite superior, valor esperado, método)
+
+        (lambda x: x, 0, 1, 0.5, "trapezios"),       # linear
+    ]
+)
+def teste_simples_funcionalidade(funcao, a, b, esperado, metodo):
+    """Teste da integral de polinômios simples"""
+    resultado = integral(funcao, a, b, n=10, metodo=metodo, plot=False)
+    assert resultado == esperado
+
+
+#Teste da Integral de Funções Constantes
+@pytest.mark.parametrize(
+    "funcao, a, b, esperado, metodo",
+    [
+        # (função, limite inferior, limite superior, valor esperado, método)
+
+        (lambda x: 1, 0, 3, 3.0, "trapezios"),       # constante
+    ]
+)
+def teste_constante(funcao, a, b, esperado, metodo):
+    """Teste da integral de funções constantes"""
+    resultado = integral(funcao, a, b, n=10, metodo=metodo, plot=False)
+    assert resultado == esperado
+
+
+
+
 
 
 
@@ -24,7 +59,8 @@ from CB2325NumericaG5 import integracao
 #Testar sem plotar gráfico
 #Testar gráfico invertido
 #Testar integral de função par e impar
-#Testar limite infinito 
+#Testar limite infinito.
+
 
 
 
