60546d5578b87580035cf726577881c4dbda7191
Davi Pestana <al.davi.campos@impatech.edu.br>
2025-11-10 11:42:24 -0300

    Andamento testes legais

---
+++ tests/test_integracao.py
@@ -0,0 +1,31 @@
+import sys, os
+import pytest
+import math
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from CB2325NumericaG5 import integracao
+
+#Testes Básicos Primeiro de Funcionamento
+
+
+
+
+
+
+
+#integral (funcao, a, b, n, aprox=4, metodo="simpson", plot=True)
+#testar intervalo não numérico
+#Testar integral imprópria, função não definida no intervalo
+#Testar n ímpar para simpson
+#testar intervalos invertidos
+#Testar n=0
+#Testar integral de constante
+#Testar integral de um ponto (intervalo de b a b)
+#Testar sem plotar gráfico
+#Testar gráfico invertido
+#Testar integral de função par e impar
+#Testar limite infinito 
+
+
+
+
